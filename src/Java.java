aff75d88686890a12c5a0d8b58d39084763f2bc4
pdhamakkar <pdhamakkar@NAGARJUNA>
2016-08-24 10:54:21 +0530

    second commit

---
+++ src/Java.java
@@ -0,0 +1,28 @@
+
+import java.util.Date;
+
+public class Java {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         System.out.println("Hello"); 
+         
+         Date date= new Date();
+         System.out.println(date.toString());
+         
+         String str = "hello";
+         System.out.println(str);
+         
+         //second commit
+         int[] num={1,2,3,4};
+         for(int i=0 ;i<num.length;i++)
+         {
+        	 System.out.println(num[i]+" ");
+         }
+         
+         
+     
+        
+	}
+
+}
